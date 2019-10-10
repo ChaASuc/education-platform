7930120eeda79e5e73d701dc23ed523e14c32093
epsilon-2048 <1099204713@qq.com>
2019-10-10 10:23:28 +0800

    修改课程模块配置文件

---
+++ ep-course-server/src/main/java/cn/ep/config/Swagger2Config.java
@@ -0,0 +1,41 @@
+package cn.ep.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Mr.Yangxiufeng
+ * Date: 2018-04-18
+ * Time: 14:52
+ */
+@Configuration
+public class Swagger2Config {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("cn.ep.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("course文档")
+                .description("course接口说明文档")
+                .termsOfServiceUrl("")
+                .contact(new Contact("陈汉槟","2042340074@qq.com","2042340074@qq.com"))
+                .version("1.0")
+                .build();
+    }
+}
