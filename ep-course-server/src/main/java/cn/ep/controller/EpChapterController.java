0183aca5aca7fdd40ace6b07ac49c93ff0e01711
epsilon-2048 <1099204713@qq.com>
2019-10-22 14:42:47 +0800

    更新域名 2019-10-22 - 14：00

--- ep-course-server/src/main/java/cn/ep/controller/EpChapterController.java
+++ ep-course-server/src/main/java/cn/ep/controller/EpChapterController.java
@@ -30,7 +30,7 @@ public class EpChapterController {
     private RedisUtil redisUtil;
 
     @ApiOperation(value = "增加【章|节】，权限：教师，可单条可批量，单条也要json数组，",notes = "开发人员已测试")
-    @ApiImplicitParam(name="chapters",value = "可单可批量(json数组传输)：chapterName，所属课程id必填，duration，如果此记录为章，可填可不填，如果为节，不填，intro，暂时无用，url，如果此纪录为章，不填，如果为节，必填，root，如果此纪录为章，则为零，如果为节，则为某章的id，free，如果课程收费，则指出这一节（对于节来说，章不填）是否收费（收费为1，免费（试看）为0），如果课程不收费，则此课程的所有章节都为零，其余的都不填",dataType = "EpChapter")
+    @ApiImplicitParam(name="chapters",value = "可单可批量(json数组传输)：chapterName、courseId必填；duration，如果此记录为章，可填可不填，如果为节，不填；intro，暂时无用；url，如果此纪录为章，不填，如果为节，必填；root，如果此纪录为章，则为零，如果为节，则为某章的id；free，如果课程收费，则指出这一节（对于节来说，章不填）是否收费（收费为1，免费（试看）为0），如果课程不收费，则此课程的所有章节都为零；其余的都不填",dataType = "EpChapter")
     @PostMapping("")
     @IsLogin
     @CanAdd(role = {RoleEnum.TEACHER})
