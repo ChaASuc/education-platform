58b027638f65a54bd5a4ab28626c3a99fcf18c2d
Geliang <2609350433@qq.com>
2019-10-30 17:52:22 +0800

    111

---
+++ ep-course-relate-common/src/main/java/cn/ep/bean/CourseComment.java
@@ -0,0 +1,65 @@
+package cn.ep.bean;
+
+import java.util.Date;
+
+public class CourseComment {
+    private Long id;
+
+    private String comment;
+
+    private Long userId;
+
+    private String userNickname;
+
+    private Long courseId;
+
+    private Date time;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment == null ? null : comment.trim();
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserNickname() {
+        return userNickname;
+    }
+
+    public void setUserNickname(String userNickname) {
+        this.userNickname = userNickname == null ? null : userNickname.trim();
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+}
---
+++ ep-course-relate-common/src/main/java/cn/ep/bean/CourseScore.java
@@ -0,0 +1,43 @@
+package cn.ep.bean;
+
+public class CourseScore {
+    private Long id;
+
+    private Integer score;
+
+    private Long userId;
+
+    private Long courseId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+}
